1039c5da26d614ad729dc38c1f5235583705f28b
Varun <63897023+varunr10@users.noreply.github.com>
2024-03-11 15:03:31 +0530

    test: add unit tests for controller

--- src/test/java/com/assignment/trainticketreservation/controllers/BookingControllerTest.java
+++ src/test/java/com/assignment/trainticketreservation/controllers/BookingControllerTest.java
@@ -1,37 +1,47 @@
 package com.assignment.trainticketreservation.controllers;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import com.assignment.trainticketreservation.entities.User;
-import com.assignment.trainticketreservation.enums.Destination;
-import com.assignment.trainticketreservation.models.BookingResponse;
-import com.assignment.trainticketreservation.models.CreateBookingRequest;
-import com.assignment.trainticketreservation.models.SeatsResponse;
-
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-@RunWith(MockitoJUnitRunner.class)
+import com.assignment.trainticketreservation.entities.User;
+import com.assignment.trainticketreservation.enums.Destination;
+import com.assignment.trainticketreservation.enums.SelectedSection;
+import com.assignment.trainticketreservation.models.BookingResponse;
+import com.assignment.trainticketreservation.models.CreateBookingRequest;
+import com.assignment.trainticketreservation.models.SeatsResponse;
+import com.assignment.trainticketreservation.services.BookingService;
+import com.assignment.trainticketreservation.services.TrainFindingService;
+import com.assignment.trainticketreservation.services.TrainService;
+
 public class BookingControllerTest {
 
-	@InjectMocks
 	BookingController controller;
-
-	@SuppressWarnings("deprecation")
-	@Before
+	BookingService bookingService;
+	
+	@BeforeEach
 	public void init() {
-		controller = new BookingController();
-		MockitoAnnotations.initMocks(this);
+		TrainService trainService = new TrainService();
+		trainService.init();
+		bookingService = new BookingService(new TrainFindingService(), trainService);
+		bookingService.init();
+		controller = new BookingController(bookingService);
 	}
 
 	@Test
 	public void testCreateBooking() {
+		CreateBookingRequest request = initBookingRequest();
+
+		BookingResponse response = controller.createBooking(request);
+		
+		Assert.assertNotNull(response.getReceiptId());
+		Assert.assertNotNull(response.getSeatNumber());
+		Assert.assertEquals(response.getSection(), SelectedSection.AC);
+	}
+
+	private CreateBookingRequest initBookingRequest() {
 		CreateBookingRequest request = new CreateBookingRequest();
 		User user = new User();
 		user.setEmail("varun123@gmail.com");
@@ -41,34 +51,50 @@ public class BookingControllerTest {
 		request.setTo(Destination.PARIS);
 		request.setUser(user);
 		request.setPricePaid(6.00);
-
-		BookingResponse response = controller.createBooking(request);
-		
-		Assert.assertNotNull(response.getReceiptId());
+		return request;
 	}
 	
 	
 	@Test
 	public void testViewBookingByReceiptId() {
 		
-		BookingResponse response = controller.viewBookingByReceipt("ABCDEG-1234511-DEFACVGH-12345");
+		//Create a booking
+		BookingResponse firstBooking = controller.createBooking(initBookingRequest());
 		
-		Assert.assertNotNull(response.getSeatNumber());
+		BookingResponse response = controller.viewBookingByReceipt(firstBooking.getReceiptId());
+		
+		Assert.assertEquals(response.getSeatNumber(), firstBooking.getSeatNumber());
+		Assert.assertEquals(response.getUsername(), firstBooking.getUsername());
 	}
 	
 	@Test
 	public void testViewAllTrainBookingBySection() {
+		testCreateBooking();
+		
 		List<SeatsResponse> responses = controller.viewAllBookingsOfTrain(11111, "AC");
 		Assert.assertTrue(!responses.isEmpty());
 	}
 	
 	@Test
 	public void testDeleteTrainBookingByUserEmail() {
+		
+		testCreateBooking();
+		
 		controller.deleteBookingByUserEmail(11111, "varun123@gmail.com");
+		
+		List<SeatsResponse> responses = controller.viewAllBookingsOfTrain(11111, "AC");
+		Assert.assertTrue(responses.isEmpty());
 	}
 	
 	@Test
 	public void testModifyUserSeat() {
+		//Create a booking
+		BookingResponse response = controller.createBooking(initBookingRequest());
+		
 		controller.modifyUserSeat(11111, "varun123@gmail.com", 5);
+		
+		BookingResponse modifiedBooking = controller.viewBookingByReceipt(response.getReceiptId());
+		
+		Assert.assertEquals(modifiedBooking.getSeatNumber(), 5);
 	}
 }
